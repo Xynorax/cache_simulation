f0276bd1a9abd74344ee682572fe6e41c802fc84
ge35woh <ge35woh@mytum.de>
2025-07-28 11:30:19 +0200

    LLC counter cache per level stats

--- .venv/Scripts/Parameters.py
+++ .venv/Scripts/Parameters.py
@@ -22,7 +22,7 @@ simulations = {}
 # Memory_size, Cache_size, Block_size, mapping, replace_policy, write_policy
 # simulations[0] = [mem_size, 2 ** 13, 2 ** 6, 2 ** 0, "LRU", "WB"]
 # simulations[1] = [mem_size, 2 ** 13, 2 ** 6, 2 ** 1, "LRU", "WB"]
-simulations[0] = [mem_size, cache_size, block_size, 2 ** 3, "ship_plus", "WB", ctr_cache_size, "LRU"]
+simulations[0] = [mem_size, cache_size, block_size, 2 ** 3, "ship_plus", "WB", ctr_cache_size, "ship_plus"]
 # simulations[3] = [mem_size, 2 ** 13, 2 ** 6, 2 ** 3, "LRU", "WB"]
 # simulations[4] = [mem_size, 2 ** 13, 2 ** 6, 2 ** 4, "LRU", "WB"]
 
@@ -85,3 +85,5 @@ level_misses_l1 = [0] * TREE_LEVELS
 level_hits = [0] * TREE_LEVELS
 level_misses = [0] * TREE_LEVELS
 level_access_counter = [0] * TREE_LEVELS
+LLC_ctr_level_hits = [0] * TREE_LEVELS
+LLC_ctr_level_misses = [0] * TREE_LEVELS
